a7cf708acb8e0def0e34be42ad3c9ec5e100bfc4
Guy <guylifshitz@gmail.com>
2023-01-20 20:57:38 +0100

    Added a seed so we can have some default entries to work with.

--- level2/message_site/db/seeds.rb
+++ level2/message_site/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+tags = Tag.create([{ name: "Urgent" }, { name: "Secret" }, { name: "Rigolo" }])
+Message.create([{ author: "piou ipou", body_pioupiou: "poup uiui piop poup uiui piop", body_french: "COUCOU", tag: tags.first }, { author: "ppou piiu", body_pioupiou: "poup poui piou piup ipuu", body_french: "CHAT", tag: tags.first }, { author: "ioui ioui ipou", body_pioupiou: "piuu piou pouu piop piup", body_french: "SALUT", tag: tags.last }])
